293cf0391e1ac6ae8f5513ef1499c84fd38780b1
banma1234 <77619465+banma1234@users.noreply.github.com>
2021-06-29 19:46:52 +0900

    1436 : 영화감독 숌
    666을 사용하는 영화의 순서를 세는 프로그램

---
+++ 2nd_week/C_666.py
@@ -0,0 +1,15 @@
+import sys
+
+def movie(n):
+    num = 666
+    count = 0
+    while True:
+        if '666' in str(num):
+            count += 1
+        if count == n:
+            break
+        num += 1
+    return num
+
+n = int(sys.stdin.readline())
+print(movie(n))
